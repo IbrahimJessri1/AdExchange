5323d5871923bce95110646e4044a30ee226e51f
IbrahimJessri <73992245+IbrahimJessri@users.noreply.github.com>
2022-08-12 19:39:12 +0300

    committed

--- config/db.py
+++ config/db.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from repositries import generics as gen
 conn = MongoClient("mongodb://localhost:27017/AdExchange")
 
 
@@ -8,3 +9,4 @@ dsp_collection = conn.AdExchange.dsp
 
 
 role_permission_collection = conn.AdExchange.role_permission
+
